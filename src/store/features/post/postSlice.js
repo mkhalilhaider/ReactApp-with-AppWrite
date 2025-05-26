e4540d5272ddd3eff40da0648c4eb9e0e2005a5d
mkhalilhaider <khalil.haider2109@gmail.com>
2025-05-26 18:48:21 +0500

    chore: configure Redux store and add auth slice, post slice scaffolded

---
+++ src/store/features/post/postSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [],
+  loading: true,
+  error: null,
+  sttaus
+};
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    createPost: (state, action) => {},
+    updatePost: (state, action) => {},
+    deletePost: (state, action) => {},
+    getPost: (state, action) => {},
+    getPosts: (state, action) => {},
+    uploadFile: (state, action) => {},
+    deleteFile: (state, action) => {},
+    getFilePreview: (state, action) => {},
+  },
+});
+
+export const {
+  createPost,
+  updatePost,
+  deletePost,
+  getPost,
+  getPosts,
+  uploadFile,
+  deleteFile,
+  getFilePreview,
+} = postSlice.actions;
+
+export default postSlice;

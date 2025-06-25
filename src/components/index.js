136d0a0b8d1cb447d2f83bd91c6fec9650da0912
mkhalilhaider <khalil.haider2109@gmail.com>
2025-06-25 08:05:29 +0500

    modified

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,21 @@ import Footer from "./Footer/Footer.jsx";
 import Logo from "./Logo.jsx";
 import Container from "./container/Container.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Login from "./Login.jsx";
+import PostCard from "./PostCard.jsx";
+import Select from "./Select.jsx";
+import Input from "./Input.jsx";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Select,
+  PostCard,
+  Login,
+  Input,
+};
